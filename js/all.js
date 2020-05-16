d760e7cb681ef248a19d62d1c9f2286dcf657d44
eloise <55773031+bananajumpjump@users.noreply.github.com>
2020-05-16 21:44:57 +0800

    new page

--- js/all.js
+++ js/all.js
@@ -1,7 +1,9 @@
 
 //引用 header / footer
 $(".headerPage").load("header.html");
+$(".headerPage").load("../header.html");
 $(".footerPage").load("footer.html");
+$(".footerPage").load("../footer.html");
 
 $(".swiper").load("swiper.html");
 
